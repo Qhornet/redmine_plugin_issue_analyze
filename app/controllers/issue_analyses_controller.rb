ba3cc4fca9df8be09d0f7d14e5ab670da299d495
mas0061 <mas0061@users.noreply.github.com>
2017-12-17 11:39:30 +0900

    * to Project module & menu * add resource for Japanese

--- app/controllers/issue_analyses_controller.rb
+++ app/controllers/issue_analyses_controller.rb
@@ -1,7 +1,13 @@
 class IssueAnalysesController < ApplicationController
   unloadable
-
+  before_filter :find_project, :authorize, :only => :index
 
   def index
+  end
+
+  private
+
+  def find_project
+    @project = Project.find(params[:project_id])
   end
 end
--- init.rb
+++ init.rb
@@ -5,4 +5,13 @@ Redmine::Plugin.register :redmine_plugin_issue_analyze do
   version '0.0.1'
   url 'https://github.com/Qhornet/redmine_plugin_issue_analyze'
   author_url 'https://github.com/Qhornet'
+
+  project_module :issue_analyses do
+    permission :issue_analyses, { :issue_analyses => 'index' }
+  end
+  menu :project_menu, :issue_analyses_menu,
+    { :controller => 'issue_analyses', :action => 'index'},
+    :caption => :issue_analyses_name,
+    :after => :gantt,
+    :param => :project_id
 end
