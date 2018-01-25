32f161db6f9c829b0e4a5ad058bc5f0378775768
mas0061 <mas0061@users.noreply.github.com>
2018-01-25 19:37:40 +0900

    test: test data of client side

--- client/src/components/AbandonedTicketsChartWrapper.vue
+++ client/src/components/AbandonedTicketsChartWrapper.vue
@@ -10,10 +10,29 @@
 <script>
 import AbandonedTicketsChart from './AbandonedTicketsChart'
 
+if (process.env.NODE_ENV !== 'production') {
+  console.log('develop')
+  var gon = {
+    leftDays: {
+      max_left_days: 10,
+      count: {
+        '0': 3,
+        '1': 5,
+        '2': 3,
+        '4': 8,
+        '10': 2
+      }
+    }
+  }
+}
+
 export default {
   name: 'abandoned-tickets-chart-wrapper',
   components: {
     AbandonedTicketsChart
+  },
+  mounted: function() {
+    console.log(gon.leftDays)
   }
 }
 </script>
