2c90e42bf620f6c053eb65c43f5554126d195ec1
mas0061 <mas0061@users.noreply.github.com>
2018-02-06 19:28:35 +0900

    feat: add selector

--- client/src/components/IssueAnalyzeView.vue
+++ client/src/components/IssueAnalyzeView.vue
@@ -2,6 +2,9 @@
   <div id="issue-analyze-view">
     <h3>{{ title }}</h3>
     <h4>放置チケットグラフ</h4>
+    <select :model="countSelected">
+      <option v-for="count in counts" value="count">{{ count }}日</option>
+    </select>
     <abandoned-tickets-chart-wrapper></abandoned-tickets-chart-wrapper>
   </div>
 </template>
@@ -9,18 +12,34 @@
 <script>
 import AbandonedTicketsChartWrapper from './AbandonedTicketsChartWrapper'
 
+console.log(process.env.NODE_ENV)
+if (process.env.NODE_ENV === 'development') {
+  var gon = {
+    leftDays: {
+      max_left_days: 10,
+      count: {
+        '0': 3,
+        '1': 5,
+        '2': 3,
+        '4': 8,
+        '10': 2
+      }
+    }
+  }
+}
+
 export default {
   name: 'issue-analyze-view',
   components: {
     AbandonedTicketsChartWrapper
   },
   data: function() {
+    const counts = ['-'].concat(Object.keys(gon.leftDays.count))
     return {
-      title: 'チケット分析'
+      title: 'チケット分析',
+      countSelected: '-',
+      counts: counts
     }
   }
 }
 </script>
-
-<style lang="css">
-</style>
